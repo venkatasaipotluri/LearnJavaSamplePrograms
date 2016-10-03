a92484615fa2148ab8bdab07a5564f26c02549f9
Venkata Sai Potluri <venkatasai.potluri@broadridge.com>
2016-10-03 15:05:58 +0530

    new HelloWorld java program

---
+++ com/javalearn/basics/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.javalearn.basics;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+
+	}
+
+}
